203b1fa251ccb3029c6134054e687f062cabc715
Atanu Nayak <93304796+Nayaker@users.noreply.github.com>
2021-11-14 23:50:16 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,5 @@
 import firebase from "firebase";
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyBT_I01oXPvZv6CLSDwoieu37WwnNrhZUI",
-    authDomain: "flirtaid-nyk.firebaseapp.com",
-    projectId: "flirtaid-nyk",
-    storageBucket: "flirtaid-nyk.appspot.com",
-    messagingSenderId: "773043219855",
-    appId: "1:773043219855:web:01f704fe10629bf5182ffa"
+//     private
 });
