5e970230d59a43f65cda89147a76a939553ce315
Atanu Nayak <93304796+Nayaker@users.noreply.github.com>
2021-12-25 23:15:40 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBCaRTKLZxzEGeH75V_oi68I67UExjX9LA",
-    authDomain: "image-uploader-nyk.firebaseapp.com",
-    projectId: "image-uploader-nyk",
-    storageBucket: "image-uploader-nyk.appspot.com",
-    messagingSenderId: "276971901234",
-    appId: "1:276971901234:web:664b4418d2262f007e072b"
+    .......
   };
 
 
